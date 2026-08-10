9f13a276b7fdb9b1bf4e31bab11037e94f0cc63a
Faizan <faizanpanwala110@gmail.com.com>
2026-08-10 12:28:03 +0530

    Backend Schema Updated

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
+  taskId: { 
+    type: String, 
+    required: true,
+    unique: true
+  },
   title: { 
     type: String, 
     required: true 
