c8f46a22838c4ebb656b9ce3a522d74fa2d4990e
Mohd-Faizan-Panwala <147788855+Mohd-Faizan-Panwala@users.noreply.github.com>
2026-08-15 16:05:49 +0530

    fix: switch to HashRouter to permanently bypass Vercel 404 routing issues

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 1. Change from BrowserRouter to HashRouter
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* 2. Wrap your app in HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
